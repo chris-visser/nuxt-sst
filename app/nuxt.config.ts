5ec720e269d93a18de16a5da276c442a2e4734e0
The Spider <2496739+chris-visser@users.noreply.github.com>
2022-07-05 09:36:49 +0200

    Remove unused deps in app

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -2,6 +2,7 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  modules: ['@nuxtjs/tailwindcss'],
   nitro: {
     preset: 'aws-lambda',
     prerender: {
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,5 @@
 <template>
   <main>
-    <NuxtLink to="/about">About</NuxtLink>
-    This is the index page!
+
   </main>
 </template>
